94262c8528f80507afdcf55a655c4e112457f367
Ana Silva <anajnsilva@gmail.com>
2025-05-11 21:05:55 -0700

    Created admin page and added functions to promote and demote users.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Joi = require("joi");
 
 const saltRounds = 12;
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const app = express();
 
 const expireTime = 1 * 60 * 60 * 1000;
@@ -30,6 +30,7 @@ app.use(express.json());
 
 app.use("/public", express.static("./public"));
 app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
 
 var mongoStore = MongoStore.create({
     mongoUrl: `mongodb+srv://${mongoUser}:${mongoPWD}@${mongoHost}/${mongoDataBase}`,
@@ -48,6 +49,40 @@ app.use(session(
 );
 
 app.set('view engine', 'ejs');
+
+function isValidSession(req) {
+    if (req.session.loggedIn) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function sessionValidation(req, res, next) {
+    if (isValidSession(req)) {
+        next();
+    } else {
+        res.redirect("/login");
+    }
+}
+
+function isAdmin(req) {
+    if (req.session.user_type == 'admin') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function adminAuthorization(req, res, next) {
+    if (!isAdmin(req)) {
+        res.status(403);
+        res.render("errorMessage", {error: "This user is not authorized to access this page."});
+        return;
+    } else {
+        next();
+    }
+}
 
 app.get('/', (req, res) => {
     let css = `<link rel="stylesheet" href="/css/index.css">`;
@@ -94,13 +129,13 @@ app.post('/submitUser', async (req, res) => {
 
     let hashedPassword = await bcrypt.hashSync(password, saltRounds);
 
-    await userCollection.insertOne({ name: name, email: email, password: hashedPassword });
+    await userCollection.insertOne({ name: name, email: email, password: hashedPassword, user_type: "user"});
     req.session.loggedIn = true;
     req.session.name = name;
     req.session.email = email;
     req.session.cookie.maxAge = expireTime;
 
-    res.render("/members", { name: name });
+    res.redirect("/members");
 });
 
 app.get('/login', (req, res) => {
@@ -123,7 +158,7 @@ app.post('/loggingIn', async (req, res) => {
         return;
     }
 
-    const result = await userCollection.find({ email: email }).project({ email: 1, password: 1, name: 1, __id: 1 }).toArray();
+    const result = await userCollection.find({ email: email }).project({ email: 1, password: 1, user_type: 1, name: 1, __id: 1 }).toArray();
     if (result.length != 1) {
         res.send("<p>Invalid email</p></br><a href='/login'>Try again</a>");
         return;
@@ -132,10 +167,11 @@ app.post('/loggingIn', async (req, res) => {
         req.session.loggedIn = true;
         req.session.name = result[0].name;
         req.session.email = email;
+        req.session.user_type = result[0].user_type;
         req.session.cookie.maxAge = expireTime;
         let css = `<link rel="stylesheet" href="/css/members.css">`;
 
-        res.render('members', {css: css, name: req.session.name});
+        res.redirect('/members');
         return;
     } else {
         res.send("<p>Incorrect password</p></br><a href='/login'>Try again</a>");
@@ -144,17 +180,28 @@ app.post('/loggingIn', async (req, res) => {
 
 });
 
-app.get('/members', (req, res) => {
-    if (!req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    } else {
+app.get('/members', sessionValidation, (req, res) => {
         let css = `<link rel="stylesheet" href="/css/members.css">`;
         let name = req.session.name;
         res.render("members", { name: name, css: css });
-    }
 });
 
+app.get('/admin', sessionValidation, adminAuthorization, async (req,res) => {
+    let result = await userCollection.find().project({name: 1, user_type: 1}).toArray();
+    res.render("admin", {users: result});
+})
+
+app.post("/promoteUser/:name", async (req, res) => {
+    let name = req.params.name;
+    await userCollection.updateOne({"name": name}, {$set : {user_type: "admin"}});
+    res.redirect('/admin');
+})
+
+app.post("/demoteUser/:name", async (req, res) => {
+    let name = req.params.name;
+    await userCollection.updateOne({"name": name}, {$set : {user_type: "user"}});
+    res.redirect('/admin');
+})
 
 app.post("/logout", function (req, res) {
 
